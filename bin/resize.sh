6894ff1988733f50d64294cef8463705c2b1ecc7
Miroslav Simko <mirsimko@gmail.com>
2019-03-12 16:36:44 +0100

    Support for float numbers in resize.sh

--- bin/resize.sh
+++ bin/resize.sh
@@ -2,15 +2,33 @@
 maxSize=${1:-2}
 echo Max size of the jpeg files is $maxSize MB
 
-for jpegFile in *.jpg; do
-  jpegSize="`du -m "$jpegFile" | awk '{print $1;}'`"
+# make it in kB
+maxSize=$( python -c "print '%d' % round($maxSize*1024)" )
+echo $maxSize
+# target directory
+mkdir -p resized
+
+# loop over all .jpg files
+for jpegFile in *.jpg *.JPG *.jpeg; do
+
+  # this is to support all JPG, jpg, and jpeg suffixes. If the file does not exist, continue
+  if [ ! -f "$jpegFile" ]; then
+    continue
+  fi
+
+  jpegSize="`du -k "$jpegFile" | awk '{print $1;}'`"
+
   if [ $jpegSize -gt $maxSize ]; then
-    # scale=$(printf %.3f $(( $jpegSize / $maxSize )) )
+    # scale for resizing in %
     scale=$( python -c "print 100.*($maxSize./$jpegSize.)" )
     # echo "print 100.*($maxSize./$jpegSize.)"
     echo Resizing "$jpegFile" to $scale % of its size
     convert -resize $scale% "$jpegFile" resized/"$jpegFile"
+  else
+    # if it is already smaller than the maxSize, just copy
+    echo "$jpegFile" already smaller than maximum size
+    cp "$jpegFile" resized
   fi
 
-  # convert -resize "$jpegFile"
 done
+# end of loop over all .jpg files
