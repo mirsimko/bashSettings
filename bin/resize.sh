7b37a160b2c6d646c401477571a2856737727ab1
Miroslav Simko <mirsimko@gmail.com>
2021-03-19 13:11:49 +0100

    script for mounting shifty

--- bin/resize.sh
+++ bin/resize.sh
@@ -3,7 +3,7 @@ maxSize=${1:-2}
 echo Max size of the jpeg files is $maxSize MB
 
 # make it in kB
-maxSize=$( python -c "print '%d' % round($maxSize*1024)" )
+maxSize=$( python -c 'print(round('$maxSize'*1024))' )
 echo $maxSize
 # target directory
 mkdir -p resized
@@ -20,7 +20,7 @@ for jpegFile in *.jpg *.JPG *.jpeg; do
 
   if [ $jpegSize -gt $maxSize ]; then
     # scale for resizing in %
-    scale=$( python -c "print 100.*($maxSize./$jpegSize.)" )
+    scale=$( python -c "print( 100.*($maxSize./$jpegSize.) )" )
     # echo "print 100.*($maxSize./$jpegSize.)"
     echo Resizing "$jpegFile" to $scale % of its size
     convert -resize $scale% "$jpegFile" resized/"$jpegFile"
