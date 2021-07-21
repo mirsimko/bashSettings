ef4fa89bbe01001ffb6194e1833570fc23b1d648
Miroslav Simko <ms@iolabs.ch>
2021-07-21 21:03:29 +0900

    set to iolabs email

--- configureGit.sh
+++ configureGit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-git config --global user.email mirsimko@gmail.com
+# git config --global user.email mirsimko@gmail.com
+git config --global user.email ms@iolabs.ch
 git config --global user.name "Miroslav Simko"
 git config --global core.editor vim
 git config --global color.ui true
